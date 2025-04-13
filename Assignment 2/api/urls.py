631752b9da441afc36422112b78d545782802f89
Indra Yudaprawira <takumu.dev@gmail.com>
2025-04-13 22:03:59 +0700

    add API URL routing for items, purchases, and sales with detail and report endpoints

---
+++ Assignment 2/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet, PurchaseHeaderViewSet, SellHeaderViewSet, PurchaseDetailListCreate, SellDetailListCreate, ReportView
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet, basename='item')
+router.register(r'purchase', PurchaseHeaderViewSet, basename='purchase')
+router.register(r'sell', SellHeaderViewSet, basename='sell')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('purchase/<str:header_code>/details/', PurchaseDetailListCreate.as_view(), name='purchase-details'),
+    path('sell/<str:header_code>/details/', SellDetailListCreate.as_view(), name='sell-details'),
+    path('report/', ReportView.as_view(), name='report'),
+]
